bff0f88cd30085ff5a6d9c5444570497bb786d58
mariasolegallina <gallinamariasole@gmail.com>
2024-04-09 00:11:14 +0200

    add db.comics + route in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('db.comics');
+
+    // dd($comics);
+
+    return view('home', compact('comics'));
 });
