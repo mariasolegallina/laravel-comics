6c172f6602f038f65707516e972ff938623c9e26
mariasolegallina <gallinamariasole@gmail.com>
2024-04-09 00:26:08 +0200

    print comics in page

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,13 +1,20 @@
 <div id="comics">
     <div class="container">
-        <h2>
-            Current Series
-        </h2>
+        <h2>Current Series</h2>
+
+        <!-- <ComicItem v-for="comic in comicsFile":comic="comic"></ComicItem> -->
+
 
         <div class="comics-list">
+            @foreach ($comics as $comicsItem)
+            <div class="comic">
+                <img class="comic-cover" src="{{ $comicsItem['thumb'] }}">
 
-            <!-- <ComicItem v-for="comic in comicsFile" :comic="comic"></ComicItem> -->
+                <span class="comic-title">{{ $comicsItem['title'] }}</span>
 
+            </div>
+
+            @endforeach
         </div>
 
         <div class="btn-container">
